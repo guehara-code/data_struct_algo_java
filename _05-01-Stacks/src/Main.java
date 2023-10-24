2408e3fa7b354b6acef7782b3f84a27fc3e12520
Gilberto <gil_uehara@yahoo.com.br>
2023-10-24 07:05:40 -0300

    more studies

--- _05-01-Stacks/src/Main.java
+++ _05-01-Stacks/src/Main.java
@@ -1,12 +1,28 @@
 
 public class Main {
     public static void main(String[] args) {
-        ArrayStack stack = new ArrayStack(10);
-        stack.push(new Employee("Jane", "Jones", 123));
-        stack.push(new Employee("John", "Doe", 4567));
-        stack.push(new Employee("Mary", "Smith", 22));
-        stack.push(new Employee("Mike", "Wilson", 3245));
-        stack.push(new Employee("Bill", "End", 78));
+
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+
+        LinkedStack stack = new LinkedStack();
+        stack.push(janeJones);
+        stack.push(johnDoe);
+        stack.push(marySmith);
+        stack.push(mikeWilson);
+        stack.push(billEnd);
+
+        stack.printStack();
+
+        System.out.println("");
+        System.out.println("Peek: " + stack.peek());
+
+        System.out.println("");
+        System.out.println("Popped: " + stack.pop());
+        System.out.println("Peek: " + stack.peek());
 
 
     }
