6a954322c038a1b3cdeae6dc0264c1dd83dc9bfc
Gilberto <gil_uehara@yahoo.com.br>
2023-10-25 19:05:42 -0300

    more studies

---
+++ _06-Queues/src/ArrayQueue.java
@@ -0,0 +1,14 @@
+public class ArrayQueue {
+
+    private Employee[] queue:
+    private int front;
+    private int back;
+
+    public ArrayQueue(int capacity) {
+        queue = new Employee[capacity];
+    }
+
+    public void add (Employee employee) {
+
+    }
+}
