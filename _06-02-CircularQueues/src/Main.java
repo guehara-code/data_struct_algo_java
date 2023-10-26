9b98a949727c26214dda4a0033574b551a45de4b
Gilberto <gil_uehara@yahoo.com.br>
2023-10-26 14:51:56 -0300

    more studies

--- _06-02-CircularQueues/src/Main.java
+++ _06-02-CircularQueues/src/Main.java
@@ -1,12 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        ArrayQueue queue = new ArrayQueue(10);
+        ArrayQueue queue = new ArrayQueue(5);
         queue.add(new Employee("Jane", "Jones", 123));
         queue.add(new Employee("John", "Doe", 4567));
         queue.add(new Employee("Mary", "Smith", 22));
         queue.add(new Employee("Mike", "Wilson", 3245));
-        queue.add(new Employee("Bill", "End", 78));
+       // queue.add(new Employee("Bill", "End", 78));
 
         queue.printQueue();
 
@@ -22,19 +22,8 @@ public class Main {
         queue.remove();
         queue.printQueue();
 
-
-        System.out.println("");
-        queue.remove();
+        queue.add(new Employee("Jane", "Jones", 123));
         queue.printQueue();
-
-
-        System.out.println("");
-        queue.remove();
-        queue.printQueue();
-
-//        System.out.println("");
-//        queue.remove();
-//        queue.printQueue();
 
         queue.add(new Employee("Jane", "Jones", 123));
         queue.printQueue();
