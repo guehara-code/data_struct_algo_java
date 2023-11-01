9bf7348a788882bc7ed102ac4bada9485f99b377
Gilberto <gil_uehara@yahoo.com.br>
2023-11-01 08:57:01 -0300

    more studies

---
+++ _08-02-BinarySearch/src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int[] intArray = { -22, -15, 1, 7, 20, 35, 55};
+        System.out.println(iterativeBinarySearch(intArray, -15));
+        System.out.println(iterativeBinarySearch(intArray, 35));
+        System.out.println(iterativeBinarySearch(intArray, 888));
+        System.out.println(iterativeBinarySearch(intArray, 1));
+    }
+
+    public static int iterativeBinarySearch(int[] input, int value) {
+        int start = 0;
+        int end = input.length;
+
+        while (start < end) {
+            int midpoint = (start + end) / 2;
+            if (input[midpoint] == value) {
+                return midpoint;
+            } else if (input[midpoint] < value) {
+                start = midpoint + 1;
+            } else {
+                end = midpoint;
+            }
+        }
+
+        return -1;
+    }
+}
