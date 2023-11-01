0c09eccf059a176a5bd7c04ca149a0810aaeb178
Gilberto <gil_uehara@yahoo.com.br>
2023-11-01 08:42:10 -0300

    more studies

--- _08-01-LinerSearch/src/Main.java
+++ _08-01-LinerSearch/src/Main.java
@@ -1,8 +1,20 @@
 
 public class Main {
     public static void main(String[] args) {
-            int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
-
-        }
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+        System.out.println(linerSearch(intArray, -15));
+        System.out.println(linerSearch(intArray, 1));
+        System.out.println(linerSearch(intArray, 888));
+        System.out.println(linerSearch(intArray, -22));
     }
+
+    public static int linerSearch(int[] input, int value) {
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
