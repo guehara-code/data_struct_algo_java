7bdc93b1aff291f7698cdc4ca57177e5ed09f5c7
Gilberto <gil_uehara@yahoo.com.br>
2023-11-01 09:03:00 -0300

    more studies

--- _08-02-BinarySearch/src/Main.java
+++ _08-02-BinarySearch/src/Main.java
@@ -6,6 +6,13 @@ public class Main {
         System.out.println(iterativeBinarySearch(intArray, 35));
         System.out.println(iterativeBinarySearch(intArray, 888));
         System.out.println(iterativeBinarySearch(intArray, 1));
+
+        System.out.println("");
+
+        System.out.println(recursiveBinarySearch(intArray, -15));
+        System.out.println(recursiveBinarySearch(intArray, 35));
+        System.out.println(recursiveBinarySearch(intArray, 888));
+        System.out.println(recursiveBinarySearch(intArray, 1));
     }
 
     public static int iterativeBinarySearch(int[] input, int value) {
@@ -25,4 +32,23 @@ public class Main {
 
         return -1;
     }
+
+    public static int recursiveBinarySearch(int[] input, int value) {
+        return recursiveBinarySearch(input, 0, input.length, value);
+    }
+
+    public static int recursiveBinarySearch(int[] input, int start, int end, int value) {
+        if (start >= end) {
+            return -1;
+        }
+        int midpoint = (start + end) / 2;
+        if (input[midpoint] == value) {
+            return midpoint;
+        } else if (input[midpoint] < value){
+            return recursiveBinarySearch(input, midpoint + 1, end, value);
+        } else {
+            return recursiveBinarySearch(input, start, midpoint, value);
+        }
+    }
+
 }
