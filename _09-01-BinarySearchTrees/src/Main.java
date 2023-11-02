1d2faa6f5d4c0cdfbafb50bce9e6b05f3bb814ce
Gilberto <gil_uehara@yahoo.com.br>
2023-11-02 09:13:13 -0300

    more studies

---
+++ _09-01-BinarySearchTrees/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Tree inTree = new Tree();
+        inTree.insert(25);
+        inTree.insert(20);
+        inTree.insert(15);
+        inTree.insert(27);
+        inTree.insert(30);
+        inTree.insert(29);
+        inTree.insert(26);
+        inTree.insert(22);
+        inTree.insert(32);
+
+    }
+}
