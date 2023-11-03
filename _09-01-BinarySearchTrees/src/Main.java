61ae985b1bb800122b3178d7a3dcc1489f85ef38
Gilberto <gil_uehara@yahoo.com.br>
2023-11-03 07:08:00 -0300

    more studies

--- _09-01-BinarySearchTrees/src/Main.java
+++ _09-01-BinarySearchTrees/src/Main.java
@@ -10,8 +10,18 @@ public class Main {
         inTree.insert(26);
         inTree.insert(22);
         inTree.insert(32);
+        inTree.insert(17);
 
         inTree.traverseInOrder();
+
+        System.out.println("");
+        System.out.println(inTree.get(27));
+        System.out.println(inTree.get(17));
+        System.out.println(inTree.get(8888));
+
+        System.out.println("");
+        System.out.println(inTree.min());
+        System.out.println(inTree.max());
 
     }
 }
