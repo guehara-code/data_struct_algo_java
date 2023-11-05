c308d69b1abdbbd85e99402a3226401b9e9c1b7c
Gilberto <gil_uehara@yahoo.com.br>
2023-11-05 07:14:12 -0300

    more studies

--- _09-01-BinarySearchTrees/src/Main.java
+++ _09-01-BinarySearchTrees/src/Main.java
@@ -25,7 +25,7 @@ public class Main {
 
         System.out.println("");
         inTree.traverseInOrder();
-        inTree.delete(25);
+        inTree.delete(15);
         System.out.println("");
         inTree.traverseInOrder();
     }
