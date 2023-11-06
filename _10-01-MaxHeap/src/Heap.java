7aca1f8d3fcb12116141636693b8b510adc7f6bd
Gilberto <gil_uehara@yahoo.com.br>
2023-11-06 09:20:38 -0300

    more studies

--- _10-01-MaxHeap/src/Heap.java
+++ _10-01-MaxHeap/src/Heap.java
@@ -6,6 +6,26 @@ public class Heap {
         heap = new int[capacity];
     }
 
+    public void insert(int value) {
+        if (isFull()) {
+            throw new IndexOutOfBoundsException("Heap is full")
+        }
+
+        heap[size] = value;
+
+        fixHeapAbove(size);
+        size++;
+    }
+
+    private void fixHeapAbove(int index) {
+        int newValue = heap[index];
+        while (index > 0 && newValue > heap[getParent(index)) {
+            heap[index] = heap[getParent(index)];
+            index = getParent(index);
+        }
+        heap[index] = newValue;
+    }
+
     public boolean isFull() {
         return size == heap.length;
     }
