56b3ecb49125ea35bf4bb5614cf1bbe5797bb5b6
Gilberto <gil_uehara@yahoo.com.br>
2023-11-09 14:36:40 -0300

    more studies

---
+++ _10-01-PriorityQueue/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main (String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+
+        pq.add(25);
+        pq.add(-22);
+        pq.add(1343);
+        pq.add(54);
+        pq.add(0);
+        pq.add(-3492);
+        pq.add(429);
+
+        System.out.println(pq.peek());
+    }
+
+}
